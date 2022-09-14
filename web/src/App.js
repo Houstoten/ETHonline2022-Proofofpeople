3d312b75aa8c8c416cb0e42d758b6dbf73c0b7e2
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2022-09-14 20:36:33 +0530

    Update frontend

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState('');
+    
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask -> https://metamask.io/");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Make sure you have metamask!');
+      return;
+    } else {
+      console.log('We have the ethereum object', ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+    } else {
+      console.log('No authorized account found');
+    }
+  };
+
+
+  const renderNotConnectedContainer = () => (
+    <div className="connect-wallet-container">
+      <button onClick={connectWallet} className="cta-button connect-wallet-button">
+        Connect Wallet
+      </button>
     </div>
   );
-}
+  
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <header>
+            <div className="left">
+              <p className="title">Proof of Protocol</p>
+              <p className="subtitle">Make you presence on-chain</p>
+              <p>Current Address: {currentAccount}</p>
+            </div>
+          </header>
+        </div>
+        
+        {!currentAccount && renderNotConnectedContainer()}
+
+        <div className="footer-container">
+           <p>Made with ❤️ by POP Team</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
